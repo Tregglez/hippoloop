edbaf258e7cdc5358edd2d70f4da05a62df500d8
Benjamin Treglown <ben@Benjamins-MacBook-Air.local>
2024-09-04 13:29:30 +1000

    updates to the navbar. Added sign in / create account button (no functionality yet) and shopping cart.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT" | "AUD" | "CAD" | "JPY",
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
+) {
+  const {currency = "AUD", notation = 'compact' } = options
+
+  const numaricPrice = typeof price === "string" ? parseFloat(price) : price
+
+  return new Intl.NumberFormat("en-AU", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2
+  }).format(numaricPrice)
+}
